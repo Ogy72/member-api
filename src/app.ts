2a1a7851872a999c93238afbd7d9a96c43f2e7f4
OgyTirta <o.tirta72@gmail.com>
2026-02-13 00:34:16 +0800

    Add Cors and URL FE

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
+import { env } from "./config/env";
 import memberRoute from "./presentation/routes/member.route";
 import authRoute from "./presentation/routes/auth.route";
 import { errorMiddleware } from "./presentation/middlewares/errror.middleware";
 import { httpLogger } from "./presentation/middlewares/logger.middleware";
 
 export const app = express();
+app.use(cors({
+    origin: env.FRONTEND_URL,
+    credentials: true,
+}));
 
 app.use(httpLogger);
 app.use(express.json());
